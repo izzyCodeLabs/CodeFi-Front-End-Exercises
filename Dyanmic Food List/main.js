d7f89747e9618726fcbfaa06a95fb7f559a91107
izzyCodeLabs <maepakeiperi@gmail.com>
2022-02-17 14:24:40 -0600

    ADD: start of dynamic food list exercise

---
+++ Dyanmic Food List/main.js
@@ -0,0 +1,22 @@
+// vars
+let menuList = document.querySelector(".menu");
+let menu = [
+    {name: "rice", price: 1.50},
+    {name: "gyoza", price: 1.50},
+    {name: "teriyaki", price: 1.50},
+    {name: "sushi", price: 1.50},
+    {name: "tempura", price: 1.50},
+    {name: "miso", price: 1.50},
+    {name: "ramen", price: 1.50},
+];
+
+// functions
+function addFood(food) {
+    const foodData = document.createElement("li");
+    foodData.innerText = food.name + ", Price: $" + food.price;
+
+}
+
+for (let i = 0; i < menu.length, i++) {
+    addFood(menu[i]);
+}
