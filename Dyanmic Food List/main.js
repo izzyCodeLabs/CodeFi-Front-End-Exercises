837d850d60facf5dcfb387056e4edfd2a37c9b7b
izzyCodeLabs <maepakeiperi@gmail.com>
2022-02-18 12:52:43 -0600

    Pushed ACTUALLY finished code to github

--- Dyanmic Food List/main.js
+++ Dyanmic Food List/main.js
@@ -1,20 +1,21 @@
 // vars
 let menuList = document.querySelector(".menu");
 let menu = [
-    {name: "rice", price: 1.50},
-    {name: "gyoza", price: 1.50},
-    {name: "teriyaki", price: 1.50},
-    {name: "sushi", price: 1.50},
-    {name: "tempura", price: 1.50},
-    {name: "miso", price: 1.50},
-    {name: "ramen", price: 1.50},
+    {name: "rice", price: 1.00},
+    {name: "gyoza", price: 3.50},
+    {name: "teriyaki", price: 6.00},
+    {name: "sushi", price: 4.00},
+    {name: "tempura", price: 3.50},
+    {name: "miso", price: 1.75},
+    {name: "ramen", price: 5.00},
 ];
 
 // functions
 function addFood(food) {
     const foodData = document.createElement("li");
-    foodData.innerText = food.name + ", Price: $" + food.price;
-
+    foodData.innerText = food.name + ", Price: $" + food.price.toFixed(2);
+    menuList.appendChild(foodData);
+    console.log(food);
 }
 
 for (let i = 0; i < menu.length; i++) {
