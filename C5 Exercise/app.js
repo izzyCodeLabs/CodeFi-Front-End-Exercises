295a4cac2c072f7c62635d281968e06b59a30e68
izzyCodeLabs <maepakeiperi@gmail.com>
2022-02-28 15:04:57 -0600

    update

---
+++ C5 Exercise/app.js
@@ -0,0 +1,13 @@
+const body = document.body;
+const input = document.querySelector(".link-input");
+const overlay = document.querySelector(".overlay");
+
+input.addEventListener("focus", focusInput);
+overlay.addEventListener("click", endFocus);
+
+function focusInput() {
+
+}
+function endFocus() {
+
+}
