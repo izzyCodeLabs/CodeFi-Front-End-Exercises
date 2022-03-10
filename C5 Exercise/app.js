1b63e59de884b777520d33d43ecc3fd009b87f9a
izzyCodeLabs <maepakeiperi@gmail.com>
2022-03-10 13:35:07 -0600

    update C5 exercise

--- C5 Exercise/app.js
+++ C5 Exercise/app.js
@@ -3,7 +3,7 @@ const input = document.querySelector(".link-input");
 const overlay = document.querySelector(".overlay");
 const form = document.querySelector("#link-form");
 const linkList = document.querySelector(".link-list");
-const allLinks = [];
+const allLinks = JSON.parse(localStorage.getItem("link_list")) || [];
 
 input.addEventListener("focus", focusInput);
 overlay.addEventListener("click", endFocus);
@@ -31,4 +31,13 @@ function createLink(e) {
     linkContainer.appendChild(newLink);
     linkList.appendChild(linkContainer);
     form.reset();
+
+    saveLinkList(allLinks);
 }
+function populateLinkList(links = []) {
+    linkList.innerHTML = links.map((link) => `<li> <a class="link" href=${link} target="_blank">${link}</a> </li>`).join("");
+}
+function saveLinkList(links = []) {
+    localStorage.setItem("link_list", JSON.stringify(links));
+}
+populateLinkList(allLinks);
