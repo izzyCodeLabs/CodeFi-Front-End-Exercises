153d5a529160cdade46a4c12e5a6393d993a3fd9
izzyCodeLabs <maepakeiperi@gmail.com>
2022-04-04 14:33:44 -0500

    add C15 Tour of Heroes

--- angular-course-project/src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ angular-course-project/src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes:Recipe[] = [
+    new Recipe('Test Recipe', 'Does it work?', 'https://img.delicious.com.au/fVd1u6k7/w1200/del/2022/02/chicken-chickpea-curry-163942-1.jpg'),
+    new Recipe('Test Recipe', 'Does it work?', 'https://img.delicious.com.au/fVd1u6k7/w1200/del/2022/02/chicken-chickpea-curry-163942-1.jpg'),
+    new Recipe('Test Recipe', 'Does it work?', 'https://img.delicious.com.au/fVd1u6k7/w1200/del/2022/02/chicken-chickpea-curry-163942-1.jpg')
+  ];
 
   constructor() { }
 
